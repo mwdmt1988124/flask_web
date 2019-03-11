aa867fb28a7394a9d239c2824bdf42afd2a9d19a
Wadehhj <344054296@qq.com>
2019-03-11 18:25:51 +0800

    unit 2

--- hello.py
+++ hello.py
@@ -1,14 +1,20 @@
 from flask import Flask
+from flask import request
+from flask import make_response
+from flask import redirect
+from flask_script import Manager
 
 app = Flask(__name__)
+manager = Manager(app)
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    user_agent = request.headers.get('User-Agent')
+    return '<h1>Your brower is %s</h1>' % user_agent
 
 @app.route('/user/<name>')
 def user(name):
     return '<h1>Hello,%s!</h1>'%name
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    manager.run()
