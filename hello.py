8a921b4a3efe389da50b62e060a6738443cb7e19
Wadehhj <344054296@qq.com>
2019-03-12 18:37:48 +0800

    unit 4(1)

--- hello.py
+++ hello.py
@@ -1,20 +1,44 @@
 from flask import Flask
 from flask import request
-from flask import make_response
-from flask import redirect
+from flask import make_response,redirect,render_template
 from flask_script import Manager
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField,SubmitField
+from wtforms.validators import Required
 
 app = Flask(__name__)
 manager = Manager(app)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+app.config['SECRET_KEY'] = 'hard to guess string'
 
-@app.route('/')
+@app.route('/',methods = ['GET','POST'])
 def index():
-    user_agent = request.headers.get('User-Agent')
-    return '<h1>Your brower is %s</h1>' % user_agent
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html',current_time =datetime.utcnow(),form = form,name=name)
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello,%s!</h1>'%name
+    return render_template('user.html',name = name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'),404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'),500
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?',validators=[Required()])
+    submit = SubmitField('Submit')
 
 if __name__ == "__main__":
     manager.run()
